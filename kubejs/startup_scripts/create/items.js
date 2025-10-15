fef2b5454e59daa148132b269cafdb288578b856
onixuniverse <warofmyhome@gmail.com>
2025-10-15 19:30:19 +0500

    v2.2.5.251015.2r

--- kubejs/startup_scripts/create/items.js
+++
@@ -1,7 +0,0 @@
-StartupEvents.registry("item", (event) => {
-    event
-      .create(`create:crushed_raw_iridium`)
-      .translationKey(`item.create.crushed_raw_iridium`)
-      .texture("mineoctober:item/crushed_raw_iridium")
-      .tag('create:crushed_raw_materials')
-});
--- kubejs/startup_scripts/item_modifications.js
+++
@@ -1,6 +0,0 @@
-ItemEvents.modification(event => {
-    event.modify('minecraft:ender_pearl', item => {
-        item.maxStackSize = 64
-        item.rarity = 'UNCOMMON'
-    });
-});
