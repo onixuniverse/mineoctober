be0f9b9223ced7e35231a06d075436601f4a2390
onixuniverse <warofmyhome@gmail.com>
2025-10-17 23:33:22 +0500

    v2.2.5

--- kubejs/server_scripts/mods/mineoctober/ore_loot.js
+++
@@ -1,6 +0,0 @@
-LootJS.modifiers(event => {
-    event.addBlockModifier('mineoctober:iridium_ore')
-         .addLoot('mineoctober:raw_iridium')
-         .removeLoot('mineoctober:iridium_ore')
-         .applyOreBonus("minecraft:fortune");
-});
